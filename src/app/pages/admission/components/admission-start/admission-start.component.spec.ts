425893d2210bfa41679df28926c73e764601b49d
Paulo Henrique Santos <paulohenriquesantos@Mac-mini-de-Paulo.local>
2025-03-05 17:38:33 -0300

    adicionando cobertura extra no form de admissao

--- src/app/pages/admission/components/admission-start/admission-start.component.spec.ts
+++ src/app/pages/admission/components/admission-start/admission-start.component.spec.ts
@@ -1,26 +1,55 @@
-import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
-import { By } from '@angular/platform-browser';
+import { AdmissionService } from '../../services/admission.service';
 import { AdmissionStartComponent } from './admission-start.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { CPF_STATUS } from '../../export/admission.model';
+import { By } from '@angular/platform-browser';
 
 describe('AdmissionStartComponent', () => {
   let component: AdmissionStartComponent;
   let fixture: ComponentFixture<AdmissionStartComponent>;
+  let admissionService: jasmine.SpyObj<AdmissionService>;
 
   beforeEach(async () => {
+    const admissionServiceSpy = jasmine.createSpyObj('AdmissionService', ['getUseByCpf']);
+
     await TestBed.configureTestingModule({
-      imports: [AdmissionStartComponent]
+      imports: [ReactiveFormsModule],
+      declarations: [],
+      providers: [
+        { provide: AdmissionService, useValue: admissionServiceSpy }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(AdmissionStartComponent);
     component = fixture.componentInstance;
+    admissionService = TestBed.inject(AdmissionService) as jasmine.SpyObj<AdmissionService>;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should submit form and handle success', fakeAsync(() => {
+    // Arrange
+    const userMock = { status: CPF_STATUS.REGULAR, name: 'John Doe' };
+    admissionService.getUseByCpf.and.returnValue(of(userMock));
+    let fakeComponent = (component as any);
+    fakeComponent.form.controls['cpf'].setValue('55154368061');
+    // Act
+    fakeComponent.onSubmit();
+    tick();
+    // Assert
+    expect(fakeComponent.isLoading()).toBeFalse();
+    expect(fakeComponent.user()).toEqual(userMock);
+    expect(fakeComponent.form.get('cpf')?.disabled).toBeTrue();
+  }));
 
   it('Should submit form', fakeAsync(() => {
     // Arrange
